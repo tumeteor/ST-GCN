21ca9d4eea557232a02b5a9d2a2a1335bef819c5
NGUYTU3 <tu.nguyen@daimler.com>
2019-08-06 14:51:13 +0200

    feature: add new fast nmf

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-import networkx as nx
+import random
+
 import numpy as np
 import argparse
 import json
 import logging
-
+import networkx as nx
 from logs import get_logger_settings, setup_logging
 from src.config import Config
-from src.reader import populate_graph_with_max_speed, read_jurbey_from_minio
-from src.nmf.fast_nmf import train_nmf
+from src.reader import populate_graph_with_max_speed, read_jurbey_from_minio, populate_graph_with_fresh_speed, \
+    get_dataframe_from_graph
+from src.nmf.fast_nmf import train_nmf_with_dataframe, train_nmf_with_sparse_matrix
 from src.graph_utils import sample_graph_by_nodes
+from src.eval.measures import rmse
 
 
 def nmf(X, K, iterations=100, alpha=0.0002, beta=0.02):
@@ -33,7 +36,7 @@ def nmf(X, K, iterations=100, alpha=0.0002, beta=0.02):
     for _iter in range(iterations):
         ii, jj = X.nonzero()
         for i, j in zip(*(ii, jj)):
-                if X[i, j] != np.nan:
+                if not np.isnan(X[i, j]):
                     eij = X[i, j] - np.dot(W[i, :], H[:, j])
                     for k in range(K):
                         # multiplicative update + L2 regularization
@@ -41,7 +44,7 @@ def nmf(X, K, iterations=100, alpha=0.0002, beta=0.02):
                         H[k][j] = H[k][j] + alpha * (2 * eij * W[i][k] - beta * H[k][j])
         tol = 0
         for i, j in zip(*(ii, jj)):
-                if X[i, j] != np.nan:
+                if not np.isnan(X[i, j]):
                     # Frobenius norm
                     tol = tol + pow(X[i, j] - np.dot(W[i, :], H[:, j]), 2)
                     for k in range(K):
@@ -71,28 +74,56 @@ if __name__ == "__main__":
     logging.info('\u2B07 Getting Jurbey File...')
     g = read_jurbey_from_minio(message['bucket'], message['jurbey_path'])
     logging.info("\u2705 Done loading Jurbey graph.")
-    # sample to sub-graph
-    g = sample_graph_by_nodes(g, 10000)
     # populate with max speed
     g = populate_graph_with_max_speed(g)
+
+    # sample to sub-graph
+    g = sample_graph_by_nodes(g, 10000)
+
+    # populate with fresh speed
+    logging.info('\u2B07 Start populating speed to the graph...')
+    g, fresh_edge_list = populate_graph_with_fresh_speed(g)
+    print(f"number of fresh edges for the sampled graph: {len(fresh_edge_list)}")
+
+    logging.info('\u2B07 Done populating speed to the graph...')
+
     # extract the adjacency matrix from graph
-    A = nx.adjacency_matrix(g, weight="speed")
-    from sklearn.preprocessing import MaxAbsScaler
+    A = nx.adjacency_matrix(g, weight="fresh_speed")
+    B = nx.adjacency_matrix(g, weight="speed")
 
-    transformer = MaxAbsScaler().fit(A)
-    A = transformer.transform(A)
+    # construct ground-truths
+    gt_edges = random.sample(fresh_edge_list, 50)
+    node_list = list(g.nodes())
+    edge_list = list(g.edges())
+    for u, v in gt_edges:
+        g[u][v]["old_speed"] = A[node_list.index(u), node_list.index(v)]
+        A[node_list.index(u), node_list.index(v)] = np.nan
+        g[u][v]["speed"] = B[node_list.index(u), node_list.index(v)]
+    W, H = nmf(X=A, K=10, iterations=2000)
 
-    # node_list = list(g.nodes())
-    # edge_list = list(g.edges())
+    y_actual = list()
+    y_pred = list()
+    y_pred_max = list()
+    for u, v in gt_edges:
+        actual = g[u][v]["old_speed"]
+        pred = np.dot(W[node_list.index(u)], H[node_list.index(v)])
+        y_actual.append(actual)
+        y_pred.append(pred)
+        y_pred_max.append(g[u][v]["speed"])
 
-    # W, H = nmf(X=A, K=10, iterations=10000)
+    # do evaluation
+    mf_rmse = rmse(y_actual, y_pred)
+    print(f'rmse for MF: {rmse}')
+    ms_rmse = rmse(y_actual, y_pred_max)
+    print(f'rmse for max speed: {ms_rmse}')
+    for x, y, z in zip(y_actual, y_pred, y_pred_max):
+        print(x, y, z)
 
-    # df = nx.to_pandas_edgelist(g)
-    # df = df[['source', 'target', 'speed']]
-    # df.rename(columns={'speed': 'weight'})
-    # df = df[0:1000]
+    # train_nmf_with_sparse_matrix(A)
+    # df = get_dataframe_from_graph(g)
+    # train_nmf_with_dataframe(df, len(df))
 
-    train_nmf(dataset=A)
+
 
 
 
--- src/nmf/fast_nmf.py
+++ src/nmf/fast_nmf.py
@@ -1,29 +1,46 @@
 from torch.utils.data import DataLoader, Dataset
+from torch.autograd import Variable as V
 import torch
 import torch.nn as nn
 import numpy as np
 
 
-class NMF(nn.Module):
-
+class NMFS(nn.Module):
     def __init__(self, num_nodes, num_factors):
         super().__init__()
         self.W = nn.Embedding(num_nodes, num_factors, sparse=True)
         self.H = nn.Embedding(num_nodes, num_factors, sparse=True)
 
-        # self.W.weight.data.uniform_(-.01, .01)
-        # self.H.weight.data.uniform_(-.01, .01)
-
         self.w_bias = nn.Embedding(num_nodes, 1, sparse=True)
         self.h_bias = nn.Embedding(num_nodes, 1, sparse=True)
-
-        # self.w_bias.weight.data.uniform_(-0.1, .01)
-        # self.h_bias.weight.data.uniform_(-0.1, .01)
 
     def forward(self, nodes):
         pred = self.w_bias(nodes) + self.h_bias(nodes)
         pred += (self.W(nodes) * self.H(nodes)).sum(dim=1, keepdim=True)
+
         return pred.squeeze()
+
+
+class NMF(nn.Module):
+    def __init__(self, in_nodes, out_nodes, num_factors):
+        super().__init__()
+        self.W = nn.Embedding(in_nodes, num_factors)
+        self.H = nn.Embedding(out_nodes, num_factors)
+
+        self.W.weight.data.uniform_(-.01, .01)
+        self.H.weight.data.uniform_(-.01, .01)
+
+        self.w_bias = nn.Embedding(in_nodes, 1)
+        self.h_bias = nn.Embedding(out_nodes, 1)
+
+        self.w_bias.weight.data.uniform_(-0.1, .01)
+        self.h_bias.weight.data.uniform_(-0.1, .01)
+
+    def forward(self, nodes):
+        in_nodes, out_nodes = nodes[:, 0], nodes[:, 1]
+        pred = (self.W(in_nodes) * self.H(out_nodes)).sum(1)
+        pred += self.w_bias(in_nodes).squeeze() + self.h_bias(out_nodes).squeeze()
+        return pred
 
 
 class SpeedDataset(Dataset):
@@ -47,8 +64,8 @@ def get_loader(df):
     return DataLoader(SpeedDataset(df), batch_size=32, num_workers=1)
 
 
-def train_nmf(dataset, num_factors=10, lr=1e-4, num_epochs=1000):
-    nmf = NMF(dataset.shape[0], num_factors)
+def train_nmf_with_sparse_matrix(dataset, num_factors=10, lr=1e-4, num_epochs=1000):
+    nmf = NMFS(dataset.shape[0], num_factors)
     optimizer = torch.optim.SparseAdam(nmf.parameters(), lr=lr)
     rows, cols = dataset.nonzero()
     p = np.random.permutation(len(rows))
@@ -75,5 +92,35 @@ def train_nmf(dataset, num_factors=10, lr=1e-4, num_epochs=1000):
         print(f"loss at step {epoch} is: {batch_loss}")
 
 
+def train_nmf_with_dataframe(dataset, num_nodes, num_factors=10, lr=1e-4, num_epochs=1000):
+    nmf = NMF(num_nodes, num_nodes, num_factors)
+    optimizer = torch.optim.SGD(nmf.parameters(), lr=lr)
+    train_loader = get_loader(dataset)
+    loss_func = torch.nn.MSELoss()
+    training_loss = []
+    for epoch in range(num_epochs):
+        for i, train_data in enumerate(train_loader):
+            # get the inputs
+            inputs = train_data.long()
+            actual_out = V(train_data[:, 2].float())
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = nmf.forward(inputs)
+
+            loss = loss_func(outputs, actual_out)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.data[0]
+
+            # input()
+            if i % (len(train_loader)) == (len(train_loader) - 1):  # print every 2000 mini-batches
+                training_loss.append((running_loss / len(train_loader)))
+                print(f"[{epoch + 1}, {i + 1}] loss: {running_loss / len(train_loader)}")
+                # epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
 
 
