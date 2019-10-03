8a0535b98723da5916f70b907a6e652739905465
NGUYTU3 <tu.nguyen@daimler.com>
2019-10-03 16:07:01 +0200

    fix: memory leak for open h5

--- src/data_loader/tensor_dataset.py
+++ src/data_loader/tensor_dataset.py
@@ -8,6 +8,7 @@ import numpy as np
 with open("src/configs/configs.yaml") as ymlfile:
     cfg = yaml.load(ymlfile)['DataConfig']
 
+
 class CustomTensorDataset(Dataset):
     def __init__(self, datasets, adj_list, mode, cluster_idx_ids, time_steps):
         self.datasets = datasets
@@ -15,16 +16,21 @@ class CustomTensorDataset(Dataset):
         self.cluster_idx_ids = cluster_idx_ids
         self.mode = mode
         self.time_steps = time_steps
+        self.prev_gidx = 0
+        self.h = h5py.File(f"data/cache/cluster_id={self.cluster_idx_ids[0]}.hdf5", "r")
 
-    @lru_cache(maxsize=128)
+    @lru_cache(maxsize=8)
     def __getitem__(self, index):
         graph_idx = int(index / self.time_steps)
         batch_idx = index % self.time_steps
         cluster_id = self.cluster_idx_ids[graph_idx]
-        h = self._load_tensor_from_path(cluster_id=cluster_id)
-        data = np.array(h["data"][self.mode][batch_idx])
-        target = np.array(h["target"][self.mode][batch_idx])
-        mask = np.array(h["mask"][self.mode][batch_idx])
+        if graph_idx != self.prev_gidx:
+            self.h.close()
+            self.h = self._load_tensor_from_path(cluster_id=cluster_id)
+            self.prev_gidx = graph_idx
+        data = np.array(self.h["data"][self.mode][batch_idx])
+        target = np.array(self.h["target"][self.mode][batch_idx])
+        mask = np.array(self.h["mask"][self.mode][batch_idx])
         return torch.from_numpy(data), torch.from_numpy(target), \
                self.adj_list[graph_idx], torch.from_numpy(mask)
 
@@ -32,8 +38,10 @@ class CustomTensorDataset(Dataset):
     def __len__(self):
         return self.time_steps * len(self.adj_list)
 
-    @lru_cache(maxsize=2048)
-    def _load_tensor_from_path(self, cluster_id):
+    @staticmethod
+    def _load_tensor_from_path(cluster_id):
         h = h5py.File(np.os.path.join(cfg['save_dir_data'], f"cluster_id={cluster_id}.hdf5", "r"))
         return h
+
+
 
