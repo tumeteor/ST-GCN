fd5d8ff4ee337107d39b1b002fdcef17bbd97d78
NGUYTU3 <tu.nguyen@daimler.com>
2019-10-21 11:45:20 +0200

    add docstrings

--- src/utils/tensor.py
+++ src/utils/tensor.py
@@ -4,13 +4,30 @@ import numpy as np
 
 
 def get_mask_from_tensor(X):
-    # Build mask tensor
+    """
+    Create a mask tensor from the original tensor, if nan -> true, else false
+    Args:
+        X (torch.Tensor) : Tensor with(out) nan values
+
+    Returns:
+        torch.BoolTensor
+    """
     X_masked = torch.where(torch.isnan(torch.from_numpy(X)), torch.tensor([0]), torch.tensor([1]))
     X_masked = X_masked.bool()
     return X_masked
 
 
 def get_mask_from_numpy_tensor(X):
+    """
+    Create a mask tensor from a numpy tensor
+    Args:
+        X (np.ndarray) : original tensor
+
+    Returns:
+        bool or ndarray of bool: Boolean result with the same shape as `x` of the NOT operation
+        on elements of `x`.
+        This is a scalar if `x` is a scalar.
+    """
     mask = ma.masked_invalid(X)
     return np.logical_not(mask.mask)
 
